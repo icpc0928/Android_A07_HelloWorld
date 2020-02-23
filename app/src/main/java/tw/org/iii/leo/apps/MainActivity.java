c527fcecdaeeacea0b79b1c25b470cc9f4ed3278
Leo <icpc0928@gmail.com>
2020-02-23 16:00:17 +0800

    02231600

--- app/src/main/java/tw/org/iii/leo/apps/MainActivity.java
+++ app/src/main/java/tw/org/iii/leo/apps/MainActivity.java
@@ -3,12 +3,34 @@ package tw.org.iii.leo.apps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.textView);
+
+    }
+
+    public void clickMe(View view) {
+        HashSet<Integer> set = new HashSet<>();
+        while (set.size()<6){
+            set.add((int)(Math.random()*49+1));
+        }
+        Log.v( "leo", set.toString());
+        tv.setText(set.toString());
+
+
+
     }
 }
